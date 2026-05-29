56f32f13574a72851a526527658d03cdd56fc5f3
hetnarowiczbeata <hetnarowiczbeata@gmail.com>
2026-05-29 08:48:47 +0200

    Dodanie historii cen paliw

--- services/orlen_fuel_price_history.py
+++ services/orlen_fuel_price_history.py
@@ -1,9 +1,14 @@
 import requests
 from datetime import datetime
 
-DATE=datetime.today().strftime('%Y-%m-%d')
-API_URL=f'https://tool.orlen.pl/api/wholesalefuelprices/ByProduct?productId=41&from=2026-01-01&to={DATE}'
-
+DATE = datetime.today().strftime('%Y-%m-%d')
+API_URL = f'https://tool.orlen.pl/api/wholesalefuelprices/ByProduct?productId=41&from=2026-01-01&to={DATE}'
 response = requests.get(API_URL)
 data = response.json()
-print(data)
+fuel_history=[]
+for item in data:
+    fuel_history.append({
+
+        'date':item['publishFrom'][:10],
+        'Product':item['value']})
+print(fuel_history)
