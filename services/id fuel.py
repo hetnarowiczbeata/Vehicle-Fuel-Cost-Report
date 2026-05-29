2f5689d23f6097fc9b1757a43b11f77a9fc4a72c
hetnarowiczbeata <hetnarowiczbeata@gmail.com>
2026-05-29 09:45:42 +0200

    products id

--- services/id fuel.py
+++ services/id fuel.py
@@ -1,21 +1,17 @@
 import requests
+def get_product_id():
+    products_id=[]
+    for product_id in range(1, 100):
+        try:
+            url = (
+                f"https://tool.orlen.pl/api/wholesalefuelprices/ByProduct"f"?productId={product_id}"f"&from=2026-01-01"f"&to=2026-05-29")
+            response = requests.get(url)
 
-for product_id in range(1, 100):
-    try:
-        url = (
-            f"https://tool.orlen.pl/api/wholesalefuelprices/ByProduct"f"?productId={product_id}"f"&from=2026-01-01"f"&to=2026-05-29")
-        response = requests.get(url)
+            if response.status_code != 200:
+                continue
+            products_id.append(product_id)
 
-        if response.status_code != 200:
-            continue
-
-        if not response.text.strip():
-            continue
-
-        data = response.json()
-
-        if data:
-            print(product_id, data[0]["productName"])
-
-    except Exception as e:
-        print(f"ID {product_id}: {e}")
+        except Exception as e:
+            print(e)
+    return products_id
+print(get_product_id())
